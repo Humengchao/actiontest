f667a9ba36d5822903101452ea72413d3ce585d2
menghu <menghu@ebay.com>
2024-07-23 13:57:43 +0800

    add an api

--- src/main/java/com/hmc/actiontest/controller/HelloController.java
+++ src/main/java/com/hmc/actiontest/controller/HelloController.java
@@ -3,11 +3,18 @@ package com.hmc.actiontest.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Time;
+
 @RestController
 public class HelloController {
 
   @RequestMapping("/hello")
   public String hello() {
     return "Hello, World!";
+  }
+
+  @RequestMapping("/time")
+  public long time() {
+    return System.currentTimeMillis();
   }
 }
